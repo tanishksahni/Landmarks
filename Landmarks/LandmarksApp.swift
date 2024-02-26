56830b0aa55161971aeee73e5117a9cb0e9542c2
tanishksahni <tanishk.sahni2002@gmail.com>
2024-02-26 15:22:11 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tanishk Sahni on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
