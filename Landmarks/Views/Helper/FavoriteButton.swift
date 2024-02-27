b6125de78bb2eae21e4eeccb3b60823a98ed26f0
tanishksahni <tanishk.sahni2002@gmail.com>
2024-02-27 10:06:48 +0530

    handling user input

---
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Tanishk Sahni on 27/02/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
