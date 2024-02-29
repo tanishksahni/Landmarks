bac19d4c994634dffd801251ac2df020ef80d951
tanishksahni <tanishk.sahni2002@gmail.com>
2024-02-29 09:33:04 +0530

    adding a badge view

---
+++ Landmarks/Views/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Tanishk Sahni on 29/02/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
